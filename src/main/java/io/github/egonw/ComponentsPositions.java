24b833f87d05ddf5a4204343027f4905f4610383
Joshiez <jhuburn@gmail.com>
2014-07-21 15:30:11 +0100

    Basic Implementation

--- src/main/java/io/github/egonw/ComponentsPositions.java
+++ src/main/java/io/github/egonw/ComponentsPositions.java
@@ -1,12 +1,48 @@
 package io.github.egonw;
 
+import java.util.Set;
+
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 
 public class ComponentsPositions {
-	
+
 	public BiMap<Integer, String> atomPositions = HashBiMap.create();
+
+	public void put(Integer position, String atomID) {
+		atomPositions.put(position, atomID);
+	}
+
+	public boolean containsValue(String value) {
+		return atomPositions.containsValue(value);
+	}
+
+	public Set keySet() {
+		return atomPositions.keySet();
+	}
 	
+	public String get(Integer key){
+		return atomPositions.get(key);
+	}
 	
+	public BiMap<String, Integer> inverse(){
+		return atomPositions.inverse();
+	}
+	
+	public void inverseAtomPositions(){
+		atomPositions.inverse();
+	}
+	
+	public int size(){
+		return atomPositions.size();
+	}
+	
+	public boolean isEmpty(){
+		return atomPositions.isEmpty();
+	}
+	
+	public void putAll(BiMap map){
+		atomPositions.putAll(map);
+	}
 
 }
