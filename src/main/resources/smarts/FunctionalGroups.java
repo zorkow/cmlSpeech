8ad2043e72adc89cae9a5505a3af6b5a46d7d150
Volker Sorge <vxs@cca-1219.cs.bham.ac.uk>
2014-07-23 12:38:23 +0100

    changed code layout.

--- src/main/resources/smarts/FunctionalGroups.java
+++ src/main/resources/smarts/FunctionalGroups.java
@@ -14,23 +14,23 @@ public class FunctionalGroups {
 
     method compute() {
       for (String group : functionalGroups) {
-             SmartsPattern pattern = SmartsPattern.create(
-                     group, bldr
-             );
+             SmartsPattern pattern = SmartsPattern.create(group, bldr);
              System.out.println("Group " + group);
              if (pattern.matches(mol)) {
-                     Mappings mappings = pattern..matchAll(mol);
-                     if (mappings.count() > 0) {
-                             Iterator<int[]> iter = mappings.iterator();
-                             while (iter.hasNext()) {
-                                     int[] matchingAtoms = iter.next();
-                                     System.out.print("  matches ");
-                                     for (int match :
-matchingAtoms) System.out.print(match + " ");
-                                     System.out.println();
-                             }
-                     }
-             }
+	       Mappings mappings = pattern..matchAll(mol);
+	       if (mappings.count() > 0) {
+		 Iterator<int[]> iter = mappings.iterator();
+		 while (iter.hasNext()) {
+		   int[] matchingAtoms = iter.next();
+		   System.out.print("  matches ");
+		   for (int match : matchingAtoms) {
+		     System.out.print(match + " ");
+		   }
+		   System.out.println();
+		 }
+	       }
+	     }
       }
     }
 }
+
