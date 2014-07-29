e803aa1bcc692ee6c7cc2e5dd7f3a82bf73a0d48
Joshiez <jhuburn@gmail.com>
2014-07-29 02:40:22 +0100

    Switched to spaces instead of tabs

--- src/main/java/io/github/egonw/ComponentsPositions.java
+++ src/main/java/io/github/egonw/ComponentsPositions.java
@@ -7,63 +7,61 @@ import com.google.common.collect.HashBiMap;
 
 public class ComponentsPositions {
 
-	// The main path through the
-	public BiMap<Integer, String> atomPositions = HashBiMap.create();
-	public int count = 0;
-	public BiMap<Integer, RichAtomSet> atomSets = HashBiMap.create();
-	
-	public BiMap<Integer, RichAtomSet> getAtomSets(){
-		return atomSets;
-	}
-	
-	// Add atom set
-	// Iterate over atom set in order (local positions)
-	
-	// Need to make heuristics to find heaviest atom
-	
-	// Find the heaviest atoms / rich atom sets
+    // The main path through the
+    public BiMap<Integer, String> atomPositions = HashBiMap.create();
+    public int count = 0;
+    public BiMap<Integer, RichAtomSet> atomSets = HashBiMap.create();
 
-	public void put(Integer position, String atomID) {
-		atomPositions.put(position, atomID);
-	}
+    public BiMap<Integer, RichAtomSet> getAtomSets() {
+        return atomSets;
+    }
 
-	public boolean containsValue(String value) {
-		return atomPositions.containsValue(value);
-	}
+    // Add atom set
+    // Iterate over atom set in order (local positions)
 
-	public Set keySet() {
-		return atomPositions.keySet();
-	}
-	
-	public String get(Integer key){
-		return atomPositions.get(key);
-	}
-	
-	public BiMap<String, Integer> inverse(){
-		return atomPositions.inverse();
-	}
-	
-	public void inverseAtomPositions(){
-		atomPositions.inverse();
-	}
-	
-	public int size(){
-		return atomPositions.size();
-	}
-	
-	public boolean isEmpty(){
-		return atomPositions.isEmpty();
-	}
-	
-	public void putAll(BiMap map){
-		atomPositions.putAll(map);
-	}
-	
-	public void addNext(String atomID){
-		atomPositions.put(count, atomID);
-		count = count++;
-	}
-	
-	
+    // Need to make heuristics to find heaviest atom
+
+    // Find the heaviest atoms / rich atom sets
+
+    public void put(Integer position, String atomID) {
+        atomPositions.put(position, atomID);
+    }
+
+    public boolean containsValue(String value) {
+        return atomPositions.containsValue(value);
+    }
+    
+    public Set keySet() {
+        return atomPositions.keySet();
+    }
+
+    public String get(Integer key) {
+        return atomPositions.get(key);
+    }
+
+    public BiMap<String, Integer> inverse() {
+        return atomPositions.inverse();
+    }
+
+    public void inverseAtomPositions() {
+        atomPositions.inverse();
+    }
+
+    public int size() {
+        return atomPositions.size();
+    }
+
+    public boolean isEmpty() {
+        return atomPositions.isEmpty();
+    }
+
+    public void putAll(BiMap map) {
+        atomPositions.putAll(map);
+    }
+
+    public void addNext(String atomID) {
+        atomPositions.put(count, atomID);
+        count = count++;
+    }
 
 }
