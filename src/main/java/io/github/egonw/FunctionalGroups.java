efd686c653cbb3867d7ef24dc0d54a2014e21e74
clegganator259 <djclegg01@hotmail.com>
2014-08-26 12:25:27 +0100

    Added the process that should occur after matching
    Added the method that turns matches into a list of RichAtomSets
    currently does not include the bonds between the atoms

--- src/main/java/io/github/egonw/FunctionalGroups.java
+++ src/main/java/io/github/egonw/FunctionalGroups.java
@@ -1,29 +1,33 @@
 package io.github.egonw;
 
+import io.github.egonw.RichAtomSet.Type;
+
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.openscience.cdk.AtomContainer;
 import org.openscience.cdk.DefaultChemObjectBuilder;
 import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.smiles.smarts.SMARTSQueryTool;
 import org.openscience.cdk.smiles.smarts.SmartsPattern;
 
-
 public class FunctionalGroups {
-	
-	public static void compute(StructuralAnalysis _analysis){
+
+	public static void compute(StructuralAnalysis _analysis) {
 		try {
-			BufferedReader br = new BufferedReader(new FileReader(new File("src/main/resources/smarts/smarts-pattern.txt")));
+			BufferedReader br = new BufferedReader(new FileReader(new File(
+					"src/main/resources/smarts/smarts-pattern.txt")));
 			String line;
-			while ((line = br.readLine()) != null){
+			while ((line = br.readLine()) != null) {
 				int colonIndex = line.indexOf(":");
-				if(colonIndex != -1 && line.charAt(0) !=  '#'){
+				if (colonIndex != -1 && line.charAt(0) != '#') {
 					String name = line.substring(0, colonIndex);
 					String pattern = line.substring(colonIndex + 2);
 					checkMollecule(pattern, name, _analysis);
@@ -31,43 +35,61 @@ public class FunctionalGroups {
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		}catch (IOException e){
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	private static void checkMollecule(String _pattern, String _name, StructuralAnalysis _analysis){
-			try {
-				IAtomContainer tempContainer = _analysis.getMolecule().clone();
-				checkMollecule(_pattern, _name, tempContainer);
-			} catch (CloneNotSupportedException e) {
-				e.printStackTrace();
-			}			
-	}
-	
-	private static void checkMollecule(String _pattern, String _name, IAtomContainer _mol){
-		try{
-		SMARTSQueryTool query = new SMARTSQueryTool(_pattern,DefaultChemObjectBuilder.getInstance());
-		boolean matchesFound = false;
+
+	private static void checkMollecule(String _pattern, String _name,
+			StructuralAnalysis _analysis) {
 		try {
-			matchesFound= query.matches(_mol);
-		} catch (CDKException e) {
-			// TODO Auto-generated catch block
+			IAtomContainer tempContainer = _analysis.getMolecule().clone();
+			checkMollecule(_pattern, _name, tempContainer);
+		} catch (CloneNotSupportedException e) {
 			e.printStackTrace();
 		}
-		if(matchesFound){
-			List<List<Integer>> mappings = query.getMatchingAtoms();
-			List<IAtomContainer> = getMappedAtoms(mappings);
-		}}catch (IllegalArgumentException e){
+	}
+
+	private static void checkMollecule(String _pattern, String _name,
+			IAtomContainer _mol) {
+		// deals with illegal smarts strings
+		try {
+		SMARTSQueryTool query = new SMARTSQueryTool(_pattern,
+				DefaultChemObjectBuilder.getInstance());
+		boolean matchesFound = false;
+			try {
+				matchesFound = query.matches(_mol);
+			} catch (CDKException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			// If there are matches, uses the getMatchingAtoms method to process
+			// the matches
+			if (matchesFound) {
+				List<List<Integer>> mappings = query.getMatchingAtoms();
+				System.out.println(_name);
+				System.out.println(_pattern);
+				List<RichAtomSet> groupList = getMappedAtoms(mappings,_mol);
+			}
+		} catch (IllegalArgumentException e) {
 			System.out.println("Error: " + _name);
 		}
 
 	}
 
-	private static List<IAtomContainer> getMappedAtoms(
-			List<List<Integer>> mappings) {
-		
-		return null;
+	private static List<RichAtomSet> getMappedAtoms(
+			List<List<Integer>> _mappings, IAtomContainer _mol) {
+		List<RichAtomSet> groups = new ArrayList<RichAtomSet>();
+		for (List<Integer> mappingList : _mappings) {
+			IAtomContainer funcGroup = new AtomContainer();
+			for (Integer i : mappingList) {
+				funcGroup.addAtom(_mol.getAtom(i));
+			}
+			RichAtomSet richFuncGroup = new RichAtomSet(funcGroup,Type.FUNCGROUP, "");//TODO FIND OUT HOW TO ADD AN ID
+			groups.add(richFuncGroup);
+			System.out.println(richFuncGroup);
+		}
+		return groups;
 	}
 }
