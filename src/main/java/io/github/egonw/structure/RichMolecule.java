6a52299d1d1693fc89acefdd3b7a3c44c82b9b27
zorkow <volker.sorge@gmail.com>
2015-04-05 21:33:40 +0100

    Adds separate dfs walking.

--- src/main/java/io/github/egonw/structure/RichMolecule.java
+++ src/main/java/io/github/egonw/structure/RichMolecule.java
@@ -45,78 +45,24 @@ import java.util.stream.Collectors;
 
 public class RichMolecule extends RichAtomSet implements RichSuperSet {
 
+    private List<RichStructure<?>> blocks = new ArrayList<>();
+
     public RichMolecule(IAtomContainer container, String id) {
         super(container, id, RichSetType.MOLECULE);
     }
 
 
-    // Maybe use later to compute majorsystems and singletonatoms.
-    //
-    // public class TreeSetSupplier implements Supplier<TreeSet<RichStructure<?>>> {
-    //     public TreeSet<RichStructure<?>> get() {
-    //         return new TreeSet<RichStructure<?>>(new WeightComparator());
-    //     }
-    // }
-    
-
-    // TODO (sorge) This should eventually become the major path computation.
     protected final void walk() {
+        this.setPath();
     }
 
     //TODO (sorge) These parameters should eventually be computed in this class!
     public final void walk(List<RichAtomSet> majorSystems, List<RichAtom> singletonAtoms) {
-        //TODO (sorge) Replace this with a sorted list
-        List<RichStructure<?>> blocks = new ArrayList<>(majorSystems);
-        blocks.addAll(singletonAtoms);
-        Collections.sort(blocks, new Heuristics(""));
-        this.walkDepthFirst(blocks);
+        this.blocks.addAll(majorSystems);
+        this.blocks.addAll(singletonAtoms);
+        this.walk();
     }
 
-
-    private class OrderComparator<T> implements Comparator<T> {
-        private List<T> order;
-
-        public OrderComparator(List<T> order) {
-            this.order = order;
-        }
-        
-        public int compare(T object1, T object2) {
-            Integer index1 = this.order.indexOf(object1);
-            Integer index2 = this.order.indexOf(object2);
-            return -1 * Integer.compare(index1, index2);
-        }
-    }
-    
-
-    /** 
-     * Depth first traversal of structure.
-     * 
-     * @param atom The start atom.
-     */
-    protected final void walkDepthFirst(List<RichStructure<?>> order) {
-        OrderComparator<RichStructure<?>> comparator = new OrderComparator<>(order);
-        if (order.isEmpty()) {
-            return;
-        }
-        RichStructure<?> first = order.get(0);
-        List<RichStructure<?>> visited =  new ArrayList<RichStructure<?>>();
-        Stack<RichStructure<?>> frontier = new Stack<RichStructure<?>>();
-        frontier.push(first);
-        while (!frontier.empty()) {
-            RichStructure<?> current = frontier.pop();
-            if (visited.contains(current)) {
-                continue;
-            }
-            visited.add(current);
-            this.path.addNext(current.getId());
-            List<RichStructure<?>> elements = current.getConnections().stream()
-                .map(con -> RichStructureHelper.getRichStructure(con.getConnected()))
-                .filter(order::contains)
-                .collect(Collectors.toList());
-            Collections.sort(elements, comparator);
-            elements.stream().forEach(frontier::push);
-        }
-    }
 
     private ComponentsPositions path = new ComponentsPositions();
 
@@ -127,6 +73,10 @@ public class RichMolecule extends RichAtomSet implements RichSuperSet {
     }
     
     @Override
-    public void setPath() {}
+    public void setPath() {
+        Collections.sort(blocks, new Heuristics(""));
+        WalkDepthFirst dfs = new WalkDepthFirst(blocks);
+        this.path = dfs.getPositions();
+    }
 
 }
