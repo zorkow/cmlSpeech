0f33bbd32f2f096fc9cca1178a6957e45819973b
zorkow <volker.sorge@gmail.com>
2015-04-19 22:48:44 +0100

    Corrects tests for complex fused systems.

--- src/test/java/com/progressiveaccess/cmlspeech/EnrichTest.java
+++ src/test/java/com/progressiveaccess/cmlspeech/EnrichTest.java
@@ -17,10 +17,10 @@
  * @file   EnrichTest.java
  * @author Volker Sorge <sorge@zorkstone>
  * @date   Thu Feb 26 17:30:05 2015
- * 
+ *
  * @brief  Full blown tests for enrichment of some standard molecules.
- * 
- * 
+ *
+ *
  */
 
 //
@@ -47,103 +47,96 @@ import org.custommonkey.xmlunit.XMLTestCase;
 
 public class EnrichTest extends XMLTestCase {
 
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public EnrichTest( String testName )
-    {
-        super( testName );
+  /**
+   * Create the test case
+   *
+   * @param testName name of the test case
+   */
+  public EnrichTest( String testName ) {
+    super( testName );
+  }
+
+
+  private static String readFile(String filename) {
+    List<String> lines = new LinkedList<String>();
+    try {
+      lines = Files.readAllLines(Paths.get(filename), StandardCharsets.UTF_8);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+    return String.join("", lines);
+  }
 
 
-    private static String readFile(String filename) {
-        List<String> lines = new LinkedList<String>();
-        try {
-            lines = Files.readAllLines(Paths.get(filename), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return String.join("", lines);
+  private void compareEnrichedMolecule(String name) {
+    System.out.println("Testing " + name + "...");
+    String[] dummy = {"-ao", "-a", "-nonih",
+                      "src/main/resources/test_files/molecule/" +
+                      name + ".mol"};
+    try {
+      App.main(dummy);
     }
-
-
-    private void compareEnrichedMolecule(String name) {
-        System.out.println("Testing " + name + "...");
-        String[] dummy = {"-ao", "-a", "-nonih",
-                          "src/main/resources/test_files/molecule/" +
-                          name + ".mol"};
-        try {
-            App.main(dummy);
-        }
-        catch (Exception e) {
-            System.out.println("Application Error: " + e.getMessage());
-            fail();
-        }
-        String original = readFile("src/test/resources/enriched/" + name + "-enr.cml");
-        String revised  = readFile(name + "-enr.cml");
-        try {
-          assertXMLEqual(name, original, revised);
-        }
-        catch (Exception e) {
-            System.out.println("XML Error " + e.getMessage());
-            fail();
-        }
+    catch (Exception e) {
+      System.out.println("Application Error: " + e.getMessage());
+      fail();
     }
-
-    /**
-     * Test enrichment of aliphatic chains.
-     */
-    @Test
-    public void testChain()
-    {
-        this.compareEnrichedMolecule("book1-004-05");
+    String original = readFile("src/test/resources/enriched/" + name + "-enr.cml");
+    String revised  = readFile(name + "-enr.cml");
+    try {
+      assertXMLEqual(name, original, revised);
     }
-
-    /**
-     * Test enrichment of functional groups.
-     */
-    @Test
-    public void testFunctional()
-    {
-        this.compareEnrichedMolecule("book1-006-03");
+    catch (Exception e) {
+      System.out.println("XML Error " + e.getMessage());
+      fail();
     }
+  }
+
+  /**
+   * Test enrichment of aliphatic chains.
+   */
+  @Test
+  public void testChain() {
+    this.compareEnrichedMolecule("book1-004-05");
+  }
+
+  /**
+   * Test enrichment of functional groups.
+   */
+  @Test
+  public void testFunctional() {
+    this.compareEnrichedMolecule("book1-006-03");
+  }
 
 
-    /**
-     * Test enrichment of ring.
-     */
-    public void testRing()
-    {
-        this.compareEnrichedMolecule("book1-012-00");
-    }
+  /**
+   * Test enrichment of ring.
+   */
+  public void testRing() {
+    this.compareEnrichedMolecule("book1-012-00");
+  }
 
 
-    /**
-     * Test enrichment of ring with functional groups.
-     */
-    public void testRingFunctional()
-    {
-        this.compareEnrichedMolecule("aspirin");
-    }
+  /**
+   * Test enrichment of ring with functional groups.
+   */
+  public void testRingFunctional() {
+    this.compareEnrichedMolecule("aspirin");
+  }
 
 
-    /**
-     * Test enrichment of complex molecule with multiple systems..
-     */
-    public void testComplex()
-    {
-      this.compareEnrichedMolecule("US06358966-20020319-C00001");
-    }
+  /**
+   * Test enrichment of complex molecule with multiple systems..
+   */
+  public void testComplex() {
+    this.compareEnrichedMolecule("US06358966-20020319-C00001");
+  }
 
 
-    /**
-     * Test enrichment of large fused ring system.
-     */
-    public void testFused()
-    {
-      this.compareEnrichedMolecule("ovalene");
-    }
+  /**
+   * Test enrichment of large fused ring system.
+   */
+  public void testFused() {
+    this.compareEnrichedMolecule("ovalene");
+  }
 
 }
