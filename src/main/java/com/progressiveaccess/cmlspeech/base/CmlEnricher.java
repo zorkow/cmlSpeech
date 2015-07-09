d7b2be20d101f8f0e125bfdf016e0f94371ff9d8
zorkow <volker.sorge@gmail.com>
2015-07-09 13:30:29 +0100

    Initial implementation of hydrogen adder.

--- src/main/java/com/progressiveaccess/cmlspeech/base/CmlEnricher.java
+++ src/main/java/com/progressiveaccess/cmlspeech/base/CmlEnricher.java
@@ -54,6 +54,14 @@ import org.xmlcml.cml.element.CMLAtomSet;
 
 import java.io.IOException;
 import java.util.List;
+import com.progressiveaccess.cmlspeech.sre.SreElement;
+import com.progressiveaccess.cmlspeech.sre.SreUtil;
+import nu.xom.Node;
+import nu.xom.Nodes;
+import java.util.stream.Collectors;
+import com.progressiveaccess.cmlspeech.sre.SreAttribute;
+import java.util.Set;
+import java.util.ArrayList;
 
 /**
  * The basic loop for semantically enriching chemical diagrams.
@@ -196,7 +204,9 @@ public class CmlEnricher {
     }
     if (Cli.hasOption("struct")) {
       SreStructure sreStructure = new SreStructure();
-      this.doc.getRootElement().appendChild(sreStructure.getAnnotations());
+      SreElement annotation = sreStructure.getAnnotations();
+      this.reattachHydrogens(annotation);
+      this.doc.getRootElement().appendChild(annotation);
     }
   }
 
@@ -260,4 +270,130 @@ public class CmlEnricher {
     }
   }
 
+
+  private void reattachHydrogens(SreElement annotation) {
+    // Get hydrogen bonds.
+    //    Compare bonds in RichStructures with all those in the CML doc.
+    // If none, quit.
+    // Add the bonds to overall structure.
+    // Add the bonds components of the non-hydrogen atom.
+    // Add a dummy node for the hydrogen atoms.
+    List<String> atoms = RichStructureHelper.getAtoms().stream()
+        .map(a -> a.getId()).collect(Collectors.toList());
+    List<String> bonds = RichStructureHelper.getBonds().stream()
+        .map(b -> b.getId()).collect(Collectors.toList());
+    Nodes nodes = SreUtil.xpathQuery(this.doc.getRootElement(), "//cml:bond");
+    System.out.println(nodes.size());
+    Integer size = 0;
+    while (size < nodes.size()) {
+      Element bond = (Element) nodes.get(size);
+      size++;
+      String bondId = bond.getAttributeValue("id");
+      if (bonds.contains(bondId)) {
+        continue;
+      }
+      String refs = bond.getAttributeValue("atomRefs2");
+      String[] parts = refs.split(" ");
+      if (parts.length < 2) {
+        continue;
+      }
+      String atomA = parts[0];
+      String atomB = parts[1];
+      if (atoms.contains(atomA)) {
+        if (atoms.contains(atomB)) {
+          continue;
+        }
+        this.insertHydrogenBond(bondId, atomB, atomA, annotation);
+        atoms.add(atomB);
+      } else {
+        this.insertHydrogenBond(bondId, atomA, atomB, annotation);
+        atoms.add(atomA);
+      }
+      bonds.add(bondId);
+      System.out.println(bondId + ": " + atomA + " " + atomB);
+    }
+  }
+
+  private void insertHydrogenBond(String bond, String hydrogen, String atom,
+                                  SreElement annotation) {
+    this.insertBond(bond, hydrogen, atom, annotation);
+    Nodes nodes = SreUtil.xpathQuery(annotation,
+        "//sre:annotation/sre:atom[.='" + atom + "']/..");
+    System.out.println("Hydrogen " + hydrogen);
+    Integer size = 0;
+    List<String> atoms = new ArrayList<String>();
+    while (size < nodes.size()) {
+      SreElement atomElement = (SreElement) nodes.get(size);
+      String parent = SreUtil.xpathValue(atomElement,
+                                         "./sre:parents/sre:atomSet");
+      System.out.println(parent);
+      System.out.println(nodes.get(size).toXML());
+      this.addBond(atomElement, bond);
+      this.insertHydrogen(bond, hydrogen, parent, annotation);
+      if (!atoms.contains(atom)) {
+        this.addToContext(bond, hydrogen, atom, annotation);
+        atoms.add(atom);
+      }
+      size++;
+    }
+  }
+
+  private void addToContext(String bond, String hydrogen, String atom,
+                            SreElement annotation) {
+    Set<String> contexts = RichStructureHelper.getRichAtom(atom).getContexts();
+    for (String context : contexts) {
+      if (!RichStructureHelper.isAtomSet(context)) {
+        continue;
+      }
+      System.out.println("Context: " + context);
+      SreElement node = (SreElement) SreUtil.xpathQueryElement(annotation,
+                                       "//sre:annotation/sre:atomSet[.='" + context + "']/..");
+      System.out.println(node.toXML());
+      SreElement component = (SreElement) SreUtil.xpathQueryElement(node,
+                                                    "./sre:component");
+      System.out.println(component.toXML());
+      component.appendChild(new SreElement(SreNamespace.Tag.BOND, bond));
+      component.appendChild(new SreElement(SreNamespace.Tag.ATOM, hydrogen));
+    }
+  }
+
+  
+  private void addBond(SreElement atom, String bond) {
+    Element component = (Element) SreUtil.xpathQueryElement(atom,
+                                                            "./sre:component");
+    component.appendChild(new SreElement(SreNamespace.Tag.BOND, bond));
+  }
+  
+  private void insertBond(String bond, String hydrogen, String atom,
+                                  SreElement annotation) {
+    SreElement element = new SreElement(SreNamespace.Tag.ANNOTATION);
+    element.appendChild(new SreElement(SreNamespace.Tag.BOND, bond));
+    element.appendChild(new SreElement(SreNamespace.Tag.PARENTS));
+    element.appendChild(new SreElement(SreNamespace.Tag.POSITION, "1"));
+    SreElement component = new SreElement(SreNamespace.Tag.COMPONENT);
+    component.appendChild(new SreElement(SreNamespace.Tag.ATOM, hydrogen));
+    component.appendChild(new SreElement(SreNamespace.Tag.ATOM, atom));
+    element.appendChild(component);
+    element.appendChild(new SreElement(SreNamespace.Tag.NEIGHBOURS));
+    annotation.appendChild(element);
+  }
+  
+  private void insertHydrogen(String bond, String hydrogen, String parent,
+                                  SreElement annotation) {
+    SreElement element = new SreElement(SreNamespace.Tag.ANNOTATION);
+    SreElement atom = new SreElement(SreNamespace.Tag.ATOM, hydrogen);
+    atom.addAttribute(new SreAttribute(SreNamespace.Attribute.TYPE,
+                                       "Hydrogen"));
+    element.appendChild(atom);
+    SreElement parents = new SreElement(SreNamespace.Tag.PARENTS);
+    parents.appendChild(new SreElement(SreNamespace.Tag.ATOMSET, parent));
+    element.appendChild(parents);
+    element.appendChild(new SreElement(SreNamespace.Tag.POSITION, "0"));
+    SreElement component = new SreElement(SreNamespace.Tag.COMPONENT);
+    component.appendChild(new SreElement(SreNamespace.Tag.BOND, bond));
+    element.appendChild(component);
+    element.appendChild(new SreElement(SreNamespace.Tag.NEIGHBOURS));
+    annotation.appendChild(element);
+  }
+  
 }
