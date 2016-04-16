f326bf5abd5eed675d10d3f9af81e8d7d2612559
zorkow <volker.sorge@gmail.com>
2016-04-17 00:56:55 +0100

    Travis testing.

--- src/test/java/com/progressiveaccess/cmlspeech/AnnotationTest.java
+++ src/test/java/com/progressiveaccess/cmlspeech/AnnotationTest.java
@@ -110,7 +110,9 @@ public abstract class AnnotationTest extends XMLTestCase {
     }
     final String original = readFile("src/test/resources/"
         + this.expectedDirectory() + "/" + name + "-enr.cml");
+    System.out.println(original);
     final String revised = readFile(name + "-enr.cml");
+    System.out.println(revised);
     try {
       this.assertXMLEqual(name, original, revised);
     } catch (final Exception e) {
